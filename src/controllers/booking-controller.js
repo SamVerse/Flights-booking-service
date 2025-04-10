9b0c274fe0d86118ac30ad2ade87e030232e8b9c
Sameer <sameersingh192004@gmail.com>
2025-04-11 01:56:54 +0530

    Made the payment confirmation API idempotent

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const { BookingService } = require('../services');
 const { ErrorResponse, SuccessResponse } = require('../utils/common');
 
+const inMemoryDB = {}; // In-memory database for demonstration purposes
 
 async function createBooking(req, res) {
     try {
@@ -24,11 +25,28 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        // Check if the idempotency key is provided in the request headers
+        // Make a idempotent API call to process the payment
+        const idempotencyKey = req.headers['x-idempotency-key'];
+        if (!idempotencyKey) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                error: 'Idempotency key is required',
+            });
+        }
+        if (inMemoryDB[idempotencyKey]) {
+            return res.status(StatusCodes.OK).json({
+                message: 'Payment already processed',
+                data: inMemoryDB[idempotencyKey],
+            });
+        }
+        // Simulate payment processing and store the result in the in-memory database
+
         const response = await BookingService.makePayment({
             bookingId: req.body.bookingId,
             userId: req.body.userId,
             totalCost: req.body.totalCost,
         })
+        inMemoryDB[idempotencyKey] = idempotencyKey; // Store the payment result using the idempotency key
         SuccessResponse.data = response;
         return res
             .status(StatusCodes.OK)
